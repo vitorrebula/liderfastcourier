35e0f69201503fc2b8ff06b1b1828e4cd9d8d3ed
Vitor Rebula Nogueira <devrebula@gmail.com>
2024-07-02 18:54:35 -0300

    Trying count

--- src/components/ContentGraph/Graphs.tsx
+++ src/components/ContentGraph/Graphs.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as styled from './Graphs.styles';
 
 function Graphs() {
+
+    const useTypewriterNumber = (end: number, duration: number) => {
+        const [count, setCount] = useState(0);
+      
+        useEffect(() => {
+          let start = 0;
+          const increment = end / (duration / 10);
+          const handle = setInterval(() => {
+            start += increment;
+            if (start >= end) {
+              clearInterval(handle);
+              setCount(end);
+            } else {
+              setCount(Math.floor(start));
+            }
+          }, 10);
+      
+          return () => clearInterval(handle);
+        }, [end, duration]);
+      
+        return count;
+    };
+
+    const deliveries = useTypewriterNumber(100000, 5000);
+    const years = useTypewriterNumber(50, 5000);
+    const states = useTypewriterNumber(15, 5000);
+    const partners = useTypewriterNumber(15, 5000);
     
     return (
         <styled.Graphs>
@@ -12,19 +39,19 @@ function Graphs() {
                     <div className="metrics-container">
                         <div className="upper">
                             <p>Entregas Realizadas</p>
-                            <h5>+100.000</h5>
+                            <h5>+{deliveries}</h5>
                         </div>
                         <div>
                             <p>Anos de Empresa</p>
-                            <h5>+50</h5>
+                            <h5>+{years}</h5>
                         </div>                
                         <div className="upper">
                             <p>Estados Atendidos</p>
-                            <h5>+15</h5>
+                            <h5>+{states}</h5>
                         </div>                
                         <div>
                             <p>Empresas Parceiras</p>
-                            <h5>+15</h5>
+                            <h5>+{partners}</h5>
                         </div>
                     </div>
                 </div>
