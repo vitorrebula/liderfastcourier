fd7db48c584f64addc6fb918e92915da13d733ba
Vitor Rebula Nogueira <devrebula@gmail.com>
2024-07-09 20:08:34 -0300

    Footer adjust

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,52 +1,42 @@
+import colors from '../../styles.colors';
 import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
-  background-color: #000;
-  color: #fff;
-  padding: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 5vh; 
+  padding: 2rem;
+  background-color: #000;
+  color: #fff;
 `;
 
-export const FooterLinks = styled.div`
+export const Logo = styled.div`
   display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  margin-bottom: 10px;
+  align-items: center;
+  cursor: pointer;
 
-  .VCK {
-    color: #fff;
-    margin: 0 5px;
-    font-size: 20px;
-    text-decoration: none;
-    cursor: pointer;
-
-    &:hover {
-      text-decoration: underline;
-    }
+  img {
+    width: 60px;
+    height: 60px;
   }
-`;
-
-export const Copyright = styled.div`
-  margin-bottom: 10px;
-  font-size: 14px;
 `;
 
 export const SocialIcons = styled.div`
   display: flex;
   justify-content: center;
+  margin-bottom: 1rem;
 
-  a {
+  svg {
+    margin: 0 0.5rem;
+    font-size: 1.5rem;
     color: #fff;
-    margin: 0 5px;
-    font-size: 20px;
-    text-decoration: none;
-    cursor: pointer;
 
     &:hover {
-      color: #ffcc00;
+      color: ${colors.orange};
     }
   }
+`;
+
+export const Copyright = styled.div`
+  font-size: 0.875rem;
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,26 @@
-import React from "react";
-import * as styled from './Footer.styles';
-import { WhatsAppOutlined, InstagramOutlined } from '@ant-design/icons';
+// Footer.tsx
+import React from 'react';
+import { InstagramOutlined } from '@ant-design/icons';
+import { FooterContainer, Logo, SocialIcons, Copyright } from './Footer.styles';
 
 function Footer() {
-
-    const goToWhatsApp = () => {
-        window.open('https://wa.me/5531999351651?text=Eu%20vim%20pelo%20site%20da%20lider!%20Gostaria%20de%20ajuda%20com%20um%20site%20ou%20marketing!!', '_blank');
+    
+    const goToLink = ({ link, type }: { link: string; type: string }) => {
+        window.open(link, type);
     }
-
     return (
-        <styled.FooterContainer>
-            <styled.Copyright>
-                © Copyright 2024 Líder Fast Courier
-            </styled.Copyright>
-            <styled.FooterLinks>
-                <div className="VCK" onClick={goToWhatsApp}>Uma produção VCK Acessoria.</div>
-            </styled.FooterLinks>
-            <styled.SocialIcons>
-                <a onClick={goToWhatsApp} target="_blank" rel="noopener noreferrer">
-                    <WhatsAppOutlined />
-                </a>
-                <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
-                    <InstagramOutlined />
-                </a>
-            </styled.SocialIcons>
-        </styled.FooterContainer>
+        <FooterContainer>
+            <Logo onClick={() => goToLink({link: 'https://www.instagram.com/digitalvck/', type: '_blank'})}>
+                <img src="./VCKLogo.jpg" alt="VCK" />
+            </Logo>
+            <SocialIcons>
+                <InstagramOutlined onClick={() => goToLink({link: 'https://www.instagram.com/vitorrebula/', type: '_blank'})}/>
+            </SocialIcons>
+            <Copyright>
+                © 2024 Líder Fast Courier. All rights reserved.
+            </Copyright>
+        </FooterContainer>
     );
-};
+}
 
 export default Footer;
